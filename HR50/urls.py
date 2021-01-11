d6f50eb63f5a619339b0967774860ff86075aeb6
IvIv2016TD <IvIv2016TD@yandex.ru>
2021-01-11 15:24:09 +0700

    Add RedirectView to HR50.urls

--- HR50/urls.py
+++ HR50/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+#Add URL maps to redirect the base URL to our application
+from django.views.generic import RedirectView
+
 urlpatterns = [
+    path( '' , RedirectView . as_view ( url = 'apphr50/' )),
 	path('apphr50/', include('apphr50.urls')),
     path('admin/', admin.site.urls),
 ]
