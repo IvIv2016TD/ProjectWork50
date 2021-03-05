b67fbffd39372f2b077106fdc816b3b621020d5f
hrwebsite <dbm19991@yandex.ru>
2021-03-05 21:34:15 +0700

    Registration with confirmation by e-mail. Console.

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 from . import views 
 from .views import SignUpView
-from accounts.views import home_view, signup_view
+from accounts.views import home_view, signup_view, activation_sent_view, activation_invalid_view, activate
 from django.contrib import admin
  
 urlpatterns = [
@@ -10,7 +10,12 @@ urlpatterns = [
     path('login/home/', views.home, name='home'),
 	path('^about_test/$', views.about_test, name='about_test'),
     path('', home_view, name="home"),
-    path('signup/', signup_view, name="signup")
+    path('signup/', signup_view, name="signup"),
+#	path('signup/', activation_sent, name="activation_sent"),
+#	path('signup/', activation_sent_view, name="activation_sent_view"),
+	path('sent/', activation_sent_view, name="activation_sent"),
+	path('signup/', activation_invalid_view, name="activation_invalid_view"),	
+	path('activate/<slug:uidb64>/<slug:token>/', activate, name="activate"),
 #	path('test_IDB_1/', views.test_IDB_1),
 #    path(r'^test_IDB/$', views.test_IDB),
 ]
