b23584cb3ed5a134e5890c31a28e7cace05b7eb6
matborge2016 <matborge2016@gmail.com>
2022-09-10 17:15:16 +0700

    Delete Heroku. Resize diagram.

--- HR50/settings.py
+++ HR50/settings.py
@@ -150,10 +150,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+## Heroku: Update database configuration from $DATABASE_URL.
+#import dj_database_url
+#db_from_env = dj_database_url.config(conn_max_age=500)
+#DATABASES['default'].update(db_from_env)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
